c27197fdc7dc9c415f18b0af31ec40a9b8604ccb
MartinG2020 <Peeniewally@users.noreply.github.com>
2020-12-07 15:59:25 +0100

    eight commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import Identicon from 'identicon.js';
-import photo from '../photo.png'
+import photo from '../photo.jpg'
 
 class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top bg-primary  flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://google.nl"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          <img src={photo} width="30" height="30" className="d-inline-block align-top mr-2"  alt="" />
           Show-Up
         </a>
         <ul className="navbar-nav px-3">
